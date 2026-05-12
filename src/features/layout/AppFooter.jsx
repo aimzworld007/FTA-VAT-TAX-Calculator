a1860661fe81d2822db110a8933d5d4bf6706021
Ainul islam <aimctgbd@gmail.com>
2026-05-12 12:04:21 +0400

    Update AppFooter.jsx

--- src/features/layout/AppFooter.jsx
+++ src/features/layout/AppFooter.jsx
@@ -11,9 +11,9 @@ export function AppFooter() {
         <RouteLink to='/documentation'>Documentation</RouteLink>
       </nav>
      <p>
-  Built with ❤️ in UAE by 
+  Built with ❤️ in UAE by  
   <a href="https://ecashbiz.com/landing" target="_blank">
-     eCashbiz Technologies
+         ~ eCashbiz Technologies
   </a>
 </p>
     </footer>
